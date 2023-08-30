0d465664333161f004a6042c762c4e9d3c4b6a73
Aditya <aditya.pandey.241103@gmail.com>
2023-08-30 13:06:05 +0530

    Minor ui fix

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -37,20 +37,18 @@ const Header = () => {
   return (
     <>
       <header
-        className={`header shadow-xl -mx-0 top-0 left-0 right-0 z-40 flex w-full items-center bg-transparent ${
-          sticky
+        className={`header shadow-xl -mx-0 top-0 left-0 right-0 z-40 flex w-full items-center bg-transparent ${sticky
             ? "!fixed !z-[9999] !bg-white !bg-opacity-80 shadow-sticky w-full backdrop-blur-sm !transition "
             : "absolute"
-        }`}
+          }`}
       >
         <div className="container">
           <div className="relative -mx-4 flex items-center justify-between">
             <div className="w-60 max-w-full px-4 md:mr-12 xl:mr-12">
               <Link
                 href="/"
-                className={`header-logo w-full flex ${
-                  sticky ? "py-5 md:12 lg:py-2" : "py-8"
-                } `}
+                className={`header-logo w-full flex ${sticky ? "py-5 md:12 lg:py-2" : "py-8"
+                  } `}
               >
                 <Image
                   src="/kietlogo.svg"
@@ -63,7 +61,6 @@ const Header = () => {
                   alt="logo"
                   width={70}
                   height={50}
-                  className=" dark:block"
                 />
               </Link>
             </div>
@@ -76,28 +73,24 @@ const Header = () => {
                   className="absolute right-4 top-1/2 block translate-y-[-50%] rounded-lg px-3 py-[6px] ring-primary focus:ring-2 lg:hidden"
                 >
                   <span
-                    className={`relative my-1.5 block h-0.5 w-[30px] bg-black transition-all duration-300 dark:bg-white ${
-                      navbarOpen ? " top-[7px] rotate-45" : " "
-                    }`}
+                    className={`relative my-1.5 block h-0.5 w-[30px] bg-black transition-all duration-300${navbarOpen ? " top-[7px] rotate-45" : " "
+                      }`}
                   />
                   <span
-                    className={`relative my-1.5 block h-0.5 w-[30px] bg-black transition-all duration-300 dark:bg-white ${
-                      navbarOpen ? "opacity-0 " : " "
-                    }`}
+                    className={`relative my-1.5 block h-0.5 w-[30px] bg-black transition-all duration-300${navbarOpen ? "opacity-0 " : " "
+                      }`}
                   />
                   <span
-                    className={`relative my-1.5 block h-0.5 w-[30px] bg-black transition-all duration-300 dark:bg-white ${
-                      navbarOpen ? " top-[-8px] -rotate-45" : " "
-                    }`}
+                    className={`relative my-1.5 block h-0.5 w-[30px] bg-black transition-all duration-300${navbarOpen ? " top-[-8px] -rotate-45" : " "
+                      }`}
                   />
                 </button>
                 <nav
                   id="navbarCollapse"
-                  className={`navbar absolute right-0 ml-[20em] z-30 w-[250px] rounded border-[.5px] border-body-color/50 bg-white py-4 px-6 duration-300 dark:border-body-color/20 dark:bg-dark lg:visible lg:static lg:w-auto lg:border-none lg:!bg-transparent lg:p-0 lg:opacity-100 ${
-                    navbarOpen
+                  className={`navbar absolute right-0 ml-[20em] z-30 w-[250px] rounded border-[.5px] border-body-color/50 bg-white py-4 px-6 duration-300 lg:visible lg:static lg:w-auto lg:border-none lg:!bg-transparent lg:p-0 lg:opacity-100 ${navbarOpen
                       ? "visibility top-full opacity-100"
                       : "invisible top-[120%] opacity-0"
-                  }`}
+                    }`}
                 >
                   <ul className="block lg:flex lg:space-x-12">
                     {menuData.map((menuItem, index) => (
@@ -105,7 +98,7 @@ const Header = () => {
                         {menuItem.path ? (
                           <Link
                             href={menuItem.path}
-                            className={`flex py-2 text-base text-dark group-hover:opacity-70 dark:text-white lg:mr-0 lg:inline-flex lg:py-6 lg:px-0`}
+                            className={`flex py-2 text-base text-dark group-hover:opacity-70 lg:mr-0 lg:inline-flex lg:py-6 lg:px-0`}
                           >
                             {menuItem.title}
                           </Link>
@@ -113,7 +106,7 @@ const Header = () => {
                           <>
                             <a
                               onClick={() => handleSubmenu(index)}
-                              className="flex cursor-pointer items-center justify-between py-2 text-base text-dark group-hover:opacity-70 dark:text-white lg:mr-0 lg:inline-flex lg:py-6 lg:px-0"
+                              className="flex cursor-pointer items-center justify-between py-2 text-base text-dark group-hover:opacity-70 lg:mr-0 lg:inline-flex lg:py-6 lg:px-0"
                             >
                               {menuItem.title}
                               <span className="pl-3">
@@ -126,15 +119,14 @@ const Header = () => {
                               </span>
                             </a>
                             <div
-                              className={`submenu relative top-full left-0 rounded-md bg-white transition-[top] duration-300 group-hover:opacity-100 dark:bg-dark lg:invisible lg:absolute lg:top-[110%] lg:block lg:w-[250px] lg:p-4 lg:opacity-0 lg:shadow-lg lg:group-hover:visible lg:group-hover:top-full ${
-                                openIndex === index ? "block" : "hidden"
-                              }`}
+                              className={`submenu relative top-full left-0 rounded-md bg-white transition-[top] duration-300 group-hover:opacity-100  lg:invisible lg:absolute lg:top-[110%] lg:block lg:w-[250px] lg:p-4 lg:opacity-0 lg:shadow-lg lg:group-hover:visible lg:group-hover:top-full ${openIndex === index ? "block" : "hidden"
+                                }`}
                             >
                               {menuItem.submenu.map((submenuItem) => (
                                 <Link
                                   href={submenuItem.path}
                                   key={submenuItem.id}
-                                  className="block rounded py-2.5 text-sm text-dark hover:opacity-70 dark:text-white lg:px-3"
+                                  className="block rounded py-2.5 text-sm text-dark hover:opacity-70 lg:px-3"
                                 >
                                   {submenuItem.title}
                                 </Link>
@@ -147,7 +139,7 @@ const Header = () => {
                   </ul>
                 </nav>
               </div>
-              
+
             </div>
           </div>
         </div>
