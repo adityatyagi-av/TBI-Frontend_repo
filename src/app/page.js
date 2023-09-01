fccf070efb6fc6bd91de1c351784c36a0460a81b
Aditya <aditya.pandey.241103@gmail.com>
2023-09-01 13:35:12 +0530

    Stats component

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import MyCarousel from '@/components/Carousel/Carousel'
 import Hero from '@/components/Hero/Hero'
-import Image from 'next/image'
+import Stats from '@/components/Stats/Stats'
+// import Image from 'next/image'
 
 
 export default function Home() {
@@ -8,8 +9,8 @@ export default function Home() {
 
     <main className="overflow-hidden font-poppins max-w-screen-xl mx-auto ">
       <Hero />
-      <h3 className="text-2xl font-semibold text-gray-800 md:text-2xl lg:text-5xl px-3 ">Our <span className='text-secondary'>Sponsors</span></h3>
       <MyCarousel />
+      <Stats />
     </main>
   )
 }
---
+++ src/components/Stats/Stats.jsx
@@ -0,0 +1,173 @@
+"use client"
+import React from 'react'
+import CountUp from 'react-countup'
+function Stats() {
+    return (
+        <section className="text-gray-600 body-font max-w-screen-xl mx-auto">
+            <div className="container px-5 py-24 mx-auto">
+                <div className="flex flex-col w-full mb-20">
+                    <h3 className="text-2xl font-semibold text-gray-800 lg:text-4xl px-3">Some <span className='text-secondary'>Statistics</span></h3>
+                </div>
+                <div className="flex flex-wrap -m-4 text-center">
+                    <div className="md:w-1/5 sm:w-1/2 w-full">
+                        <div className="bg-secondary text-white px-4 py-6">
+                            <div className='font-medium text-3xl mb-5'>
+                                <CountUp start={0} end={1000} duration={2} suffix='+' />
+                            </div>
+                            <p className="leading-relaxed">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, iusto.</p>
+                        </div>
+                    </div>
+                    <div className="md:w-1/5 sm:w-1/2 w-full">
+                        <div className="bg-white px-4 py-6">
+                            <div className='font-medium text-3xl mb-5'>
+                                <CountUp start={0} end={1000} duration={2} suffix='+' />
+                            </div>
+                            <p className="leading-relaxed">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, officiis.</p>
+                        </div>
+                    </div>
+                    <div className="md:w-1/5 sm:w-1/2 w-full">
+                        <div className="bg-secondary text-white px-4 py-6">
+                            <div className='font-medium text-3xl mb-5'>
+                                <CountUp start={0} end={1000} duration={2} suffix='+' />
+                            </div>
+                            <p className="leading-relaxed">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, iusto.</p>
+                        </div>
+                    </div>
+                    <div className="md:w-1/5 sm:w-1/2 w-full">
+                        <div className="bg-white px-4 py-6">
+                            <div className='font-medium text-3xl mb-5'>
+                                <CountUp start={0} end={1000} duration={2} suffix='+' />
+                            </div>
+                            <p className="leading-relaxed">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, officiis.</p>
+                        </div>
+                    </div> <div className="md:w-1/5 sm:w-1/2 w-full">
+                        <div className="bg-secondary text-white px-4 py-6">
+                            <div className='font-medium text-3xl mb-5'>
+                                <CountUp start={0} end={1000} duration={2} suffix='+' />
+                            </div>
+                            <p className="leading-relaxed">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, iusto.</p>
+                        </div>
+                    </div>
+                    <div className="md:w-1/5 sm:w-1/2 w-full">
+                        <div className="bg-white px-4 py-6">
+                            <div className='font-medium text-3xl mb-5'>
+                                <CountUp start={0} end={1000} duration={2} suffix='+' />
+                            </div>
+                            <p className="leading-relaxed">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, officiis.</p>
+                        </div>
+                    </div> <div className="md:w-1/5 sm:w-1/2 w-full">
+                        <div className="bg-secondary text-white px-4 py-6">
+                            <div className='font-medium text-3xl mb-5'>
+                                <CountUp start={0} end={1000} duration={2} suffix='+' />
+                            </div>
+                            <p className="leading-relaxed">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, iusto.</p>
+                        </div>
+                    </div>
+                    <div className="md:w-1/5 sm:w-1/2 w-full">
+                        <div className="bg-white px-4 py-6">
+                            <div className='font-medium text-3xl mb-5'>
+                                <CountUp start={0} end={1000} duration={2} suffix='+' />
+                            </div>
+                            <p className="leading-relaxed">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, officiis.</p>
+                        </div>
+                    </div> <div className="md:w-1/5 sm:w-1/2 w-full">
+                        <div className="bg-secondary text-white px-4 py-6">
+                            <div className='font-medium text-3xl mb-5'>
+                                <CountUp start={0} end={1000} duration={2} suffix='+' />
+                            </div>
+                            <p className="leading-relaxed">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, iusto.</p>
+                        </div>
+                    </div>
+                    <div className="md:w-1/5 sm:w-1/2 w-full">
+                        <div className="bg-white px-4 py-6">
+                            <div className='font-medium text-3xl mb-5'>
+                                <CountUp start={0} end={1000} duration={2} suffix='+' />
+                            </div>
+                            <p className="leading-relaxed">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, officiis.</p>
+                        </div>
+                    </div>
+                    <div className="md:w-1/5 sm:w-1/2 w-full">
+                        <div className="bg-secondary text-white px-4 py-6">
+                            <div className='font-medium text-3xl mb-5'>
+                                <CountUp start={0} end={1000} duration={2} suffix='+' />
+                            </div>
+                            <p className="leading-relaxed">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, iusto.</p>
+                        </div>
+                    </div>
+                    <div className="md:w-1/5 sm:w-1/2 w-full">
+                        <div className="bg-white px-4 py-6">
+                            <div className='font-medium text-3xl mb-5'>
+                                <CountUp start={0} end={1000} duration={2} suffix='+' />
+                            </div>
+                            <p className="leading-relaxed">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, officiis.</p>
+                        </div>
+                    </div>
+                    <div className="md:w-1/5 sm:w-1/2 w-full">
+                        <div className="bg-secondary text-white px-4 py-6">
+                            <div className='font-medium text-3xl mb-5'>
+                                <CountUp start={0} end={1000} duration={2} suffix='+' />
+                            </div>
+                            <p className="leading-relaxed">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, iusto.</p>
+                        </div>
+                    </div>
+                    <div className="md:w-1/5 sm:w-1/2 w-full">
+                        <div className="bg-white px-4 py-6">
+                            <div className='font-medium text-3xl mb-5'>
+                                <CountUp start={0} end={1000} duration={2} suffix='+' />
+                            </div>
+                            <p className="leading-relaxed">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, officiis.</p>
+                        </div>
+                    </div> <div className="md:w-1/5 sm:w-1/2 w-full">
+                        <div className="bg-secondary text-white px-4 py-6">
+                            <div className='font-medium text-3xl mb-5'>
+                                <CountUp start={0} end={1000} duration={2} suffix='+' />
+                            </div>
+                            <p className="leading-relaxed">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, iusto.</p>
+                        </div>
+                    </div>
+                    <div className="md:w-1/5 sm:w-1/2 w-full">
+                        <div className="bg-white px-4 py-6">
+                            <div className='font-medium text-3xl mb-5'>
+                                <CountUp start={0} end={1000} duration={2} suffix='+' />
+                            </div>
+                            <p className="leading-relaxed">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, officiis.</p>
+                        </div>
+                    </div> <div className="md:w-1/5 sm:w-1/2 w-full">
+                        <div className="bg-secondary text-white px-4 py-6">
+                            <div className='font-medium text-3xl mb-5'>
+                                <CountUp start={0} end={1000} duration={2} suffix='+' />
+                            </div>
+                            <p className="leading-relaxed">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, iusto.</p>
+                        </div>
+                    </div>
+                    <div className="md:w-1/5 sm:w-1/2 w-full">
+                        <div className="bg-white px-4 py-6">
+                            <div className='font-medium text-3xl mb-5'>
+                                <CountUp start={0} end={1000} duration={2} suffix='+' />
+                            </div>
+                            <p className="leading-relaxed">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, officiis.</p>
+                        </div>
+                    </div> <div className="md:w-1/5 sm:w-1/2 w-full">
+                        <div className="bg-secondary text-white px-4 py-6">
+                            <div className='font-medium text-3xl mb-5'>
+                                <CountUp start={0} end={1000} duration={2} suffix='+' />
+                            </div>
+                            <p className="leading-relaxed">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, iusto.</p>
+                        </div>
+                    </div>
+                    <div className="md:w-1/5 sm:w-1/2 w-full">
+                        <div className="bg-white px-4 py-6">
+                            <div className='font-medium text-3xl mb-5'>
+                                <CountUp start={0} end={1000} duration={2} suffix='+' />
+                            </div>
+                            <p className="leading-relaxed">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, officiis.</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Stats
