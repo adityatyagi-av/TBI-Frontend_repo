565403572ff0cf16ae95e823b764f09bebb1797c
Aditya <aditya.pandey.241103@gmail.com>
2023-09-01 12:21:11 +0530

    minor fix

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
 
-    <main className="overflow-hidden font-poppins max-w-screen-2xl mx-auto ">
+    <main className="overflow-hidden font-poppins max-w-screen-xl mx-auto ">
       <Hero />
       <h3 className="text-2xl font-semibold text-gray-800 md:text-2xl lg:text-5xl px-3 ">Our <span className='text-secondary'>Sponsors</span></h3>
       <MyCarousel />
