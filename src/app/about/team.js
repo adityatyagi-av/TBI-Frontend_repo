f5b137ca7fa25b5d7c083f647caab0d8ab3fee76
ADITYA TYAGI <adityatyagiav1@gmail.com>
2023-09-01 20:39:02 +0530

    individual card template designed

---
+++ src/app/about/team.js
@@ -0,0 +1,72 @@
+const mainteam=[
+    {
+        name:"Our Lead Team",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo incidunt ex placeat modi magni quia error alias, adipisci rem similique, at omnis eligendi optio eos harum.",
+        members:[
+            {
+                id:1,
+                name:"Saurav Kumar",
+                imagesrc:"",
+                linkedin:"",
+                twitter:"",
+                github:"",
+            }
+        ]
+    
+    }
+
+]
+const developerteam=[
+    {
+        name:"Our Lead Team",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo incidunt ex placeat modi magni quia error alias, adipisci rem similique, at omnis eligendi optio eos harum.",
+        members:[
+            {
+                id:1,
+                name:"Saurav Kumar",
+                imagesrc:"",
+                linkedin:"",
+                twitter:"",
+                github:"",
+            }
+        ]
+    
+    }
+
+]
+const designteam=[
+    {
+        name:"Our Lead Team",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo incidunt ex placeat modi magni quia error alias, adipisci rem similique, at omnis eligendi optio eos harum.",
+        members:[
+            {
+                id:1,
+                name:"Saurav Kumar",
+                imagesrc:"",
+                linkedin:"",
+                twitter:"",
+                github:"",
+            }
+        ]
+    
+    }
+
+]
+const marketingteam=[
+    {
+        name:"Our Lead Team",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo incidunt ex placeat modi magni quia error alias, adipisci rem similique, at omnis eligendi optio eos harum.",
+        members:[
+            {
+                id:1,
+                name:"Saurav Kumar",
+                imagesrc:"",
+                linkedin:"",
+                twitter:"",
+                github:"",
+            }
+        ]
+    
+    }
+
+]
