d2ba4113249688c7026e689d2e94bc50d8b0166b
ADITYA TYAGI <adityatyagiav1@gmail.com>
2023-09-01 22:39:43 +0530

    teamcard completely designed

--- src/app/about/team.js
+++ src/app/about/team.js
@@ -5,12 +5,20 @@ const mainteam=[
         members:[
             {
                 id:1,
-                name:"Saurav Kumar",
-                imagesrc:"",
-                linkedin:"",
-                twitter:"",
-                github:"",
-            }
+                name:"Aditya Tyagi",
+                imagesrc:"/aditya.png",
+                linkedin:"https://www.linkedin.com/in/adityatyagiav/",
+                twitter:"https://twitter.com/adityatyagiav",
+                github:"https://github.com/adityatyagi-av",
+            },
+            {
+                id:2,
+                name:"Aditya Pandey",
+                imagesrc:"/pandey.jpeg",
+                linkedin:"https://www.linkedin.com/in/adityyaa10/",
+                twitter:"https://twitter.com/adityyaa_10",
+                github:"https://github.com/adityyaa-10",
+            },
         ]
     
     }
@@ -18,17 +26,43 @@ const mainteam=[
 ]
 const developerteam=[
     {
-        name:"Our Lead Team",
+        name:"Our Development Team",
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo incidunt ex placeat modi magni quia error alias, adipisci rem similique, at omnis eligendi optio eos harum.",
         members:[
             {
                 id:1,
-                name:"Saurav Kumar",
-                imagesrc:"",
-                linkedin:"",
-                twitter:"",
-                github:"",
-            }
+                name:"Aditya Pandey",
+                imagesrc:"/pandey.jpeg",
+                linkedin:"https://www.linkedin.com/in/adityyaa10/",
+                twitter:"https://twitter.com/adityyaa_10",
+                github:"https://github.com/adityyaa-10",
+            },
+            {
+                id:2,
+                name:"Aditya Tyagi",
+                imagesrc:"/aditya.png",
+                linkedin:"https://www.linkedin.com/in/adityatyagiav/",
+                twitter:"https://twitter.com/adityatyagiav",
+                github:"https://github.com/adityatyagi-av",
+            },
+            {
+                id:3,
+                name:"Devyanshi Srivastava",
+                imagesrc:"/devyanshi.jpg",
+                linkedin:"https://www.linkedin.com/in/devyanshiiii21/",
+                twitter:"https://twitter.com/devyanshi_21",
+                github:"https://github.com/devyanshiiii21",
+            },
+            {
+                id:4,
+                name:"Shivani Rajput",
+                imagesrc:"/shivani.png",
+                linkedin:"https://www.linkedin.com/in/shivani-rajput-b54785232/",
+                twitter:"https://twitter.com/devyanshi_21",
+                github:"https://github.com/shivaniirajput",
+            },
+            
+
         ]
     
     }
@@ -41,12 +75,20 @@ const designteam=[
         members:[
             {
                 id:1,
-                name:"Saurav Kumar",
-                imagesrc:"",
-                linkedin:"",
-                twitter:"",
-                github:"",
-            }
+                name:"Aditya Tyagi",
+                imagesrc:"/aditya.png",
+                linkedin:"https://www.linkedin.com/in/adityatyagiav/",
+                twitter:"https://twitter.com/adityatyagiav",
+                github:"https://github.com/adityatyagi-av",
+            },
+            {
+                id:2,
+                name:"Aditya Pandey",
+                imagesrc:"/pandey.jpeg",
+                linkedin:"https://www.linkedin.com/in/adityyaa10/",
+                twitter:"https://twitter.com/adityyaa_10",
+                github:"https://github.com/adityyaa-10",
+            },
         ]
     
     }
@@ -59,14 +101,23 @@ const marketingteam=[
         members:[
             {
                 id:1,
-                name:"Saurav Kumar",
-                imagesrc:"",
-                linkedin:"",
-                twitter:"",
-                github:"",
-            }
+                name:"Aditya Tyagi",
+                imagesrc:"/aditya.png",
+                linkedin:"https://www.linkedin.com/in/adityatyagiav/",
+                twitter:"https://twitter.com/adityatyagiav",
+                github:"https://github.com/adityatyagi-av",
+            },
+            {
+                id:2,
+                name:"Aditya Pandey",
+                imagesrc:"/pandey.jpeg",
+                linkedin:"https://www.linkedin.com/in/adityyaa10/",
+                twitter:"https://twitter.com/adityyaa_10",
+                github:"https://github.com/adityyaa-10",
+            },
         ]
     
     }
 
 ]
+export {mainteam,developerteam,designteam,marketingteam}
--- src/app/about/teams.jsx
+++ src/app/about/teams.jsx
@@ -1,7 +1,8 @@
 'use client'
 import React from "react";
 import {Tabs, Tab, Card, CardBody} from "@nextui-org/react";
-
+import { mainteam , developerteam , designteam , marketingteam } from "./team";
+import TeamCard from "./teamcards";
 
 
 const Teams = () => {
@@ -9,17 +10,17 @@ const Teams = () => {
     <div className="flex  flex-col mx-10 ">
       <Tabs aria-label="Options" fullWidth={true} size="lg"  className=" mx-auto mb-5 py-3">
         <Tab key="mainteam" title="Main Team">
-            
+           <TeamCard team={mainteam[0]}/>
         </Tab>
         <Tab key="development" title="Development">
-          
+        <TeamCard team={developerteam[0]}/>
         </Tab>
         
         <Tab key="design" title="Design">
-           
+        <TeamCard team={designteam[0]}/>
         </Tab>
         <Tab key="marketing" title="Marketing">
-          
+        <TeamCard team={marketingteam[0]}/>
         </Tab>
         
       </Tabs>
