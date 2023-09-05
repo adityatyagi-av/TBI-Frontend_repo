19c889e26e4940922283e357921b03e28841e754
Shivani <Shivanirajpoot998@gmail.com>
2023-09-05 11:20:49 +0530

    Design improved

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['https://merakiui.com'],
+        domains: ['https://merakiui.com','i.ibb.co',],
       },
 }
 
