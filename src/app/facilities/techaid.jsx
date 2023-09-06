002915970133f56f1c0fc31184fa05deaa6a6efd
ADITYA TYAGI <adityatyagiav1@gmail.com>
2023-09-06 10:42:34 +0530

    facilities page redesigned

--- src/app/facilities/techaid.jsx
+++ src/app/facilities/techaid.jsx
@@ -2,9 +2,407 @@ import React from 'react'
 
 const TechAid = () => {
   return (
-    <div>
-      
+    <>
+   
+    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="grid gap-10 lg:grid-cols-2">
+        <div className="flex flex-col justify-center md:pr-8 xl:pr-0 lg:max-w-lg">
+          <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-teal-accent-400">
+            <svg className="text-teal-900 w-7 h-7" viewBox="0 0 24 24">
+              <polyline
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeMiterlimit="10"
+                points=" 8,5 8,1 16,1 16,5"
+                strokeLinejoin="round"
+              />
+              <polyline
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeMiterlimit="10"
+                points="9,15 1,15 1,5 23,5 23,15 15,15"
+                strokeLinejoin="round"
+              />
+              <polyline
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeMiterlimit="10"
+                points="22,18 22,23 2,23 2,18"
+                strokeLinejoin="round"
+              />
+              <rect
+                x="9"
+                y="13"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeMiterlimit="10"
+                width="6"
+                height="4"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </div>
+          <div className="max-w-xl mb-6">
+            <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+              Let us handle
+              <br className="hidden md:block" />
+              your next{' '}
+              <span className="inline-block text-deep-purple-accent-400">
+                destination
+              </span>
+            </h2>
+            <p className="text-base text-gray-700 md:text-lg">
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae. explicabo.
+            </p>
+          </div>
+          <div>
+            <a
+              href="/"
+              aria-label=""
+              className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+            >
+              Learn more
+              <svg
+                className="inline-block w-3 ml-2"
+                fill="currentColor"
+                viewBox="0 0 12 12"
+              >
+                <path d="M9.707,5.293l-5-5A1,1,0,0,0,3.293,1.707L7.586,6,3.293,10.293a1,1,0,1,0,1.414,1.414l5-5A1,1,0,0,0,9.707,5.293Z" />
+              </svg>
+            </a>
+          </div>
+        </div>
+        <div className="flex items-center justify-center -mx-4 lg:pl-8">
+          <div className="flex flex-col items-end px-3">
+            <img
+              className="object-cover mb-6 rounded shadow-lg h-28 sm:h-48 xl:h-56 w-28 sm:w-48 xl:w-56"
+              src="https://images.pexels.com/photos/3184287/pexels-photo-3184287.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+              alt=""
+            />
+            <img
+              className="object-cover w-20 h-20 rounded shadow-lg sm:h-32 xl:h-40 sm:w-32 xl:w-40"
+              src="https://images.pexels.com/photos/3182812/pexels-photo-3182812.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+              alt=""
+            />
+          </div>
+          <div className="px-3">
+            <img
+              className="object-cover w-40 h-40 rounded shadow-lg sm:h-64 xl:h-80 sm:w-64 xl:w-80"
+              src="https://images.pexels.com/photos/3182739/pexels-photo-3182739.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;w=500"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
     </div>
+    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-5">
+        <div className="grid gap-6 md:grid-cols-2 md:col-span-2 lg:col-span-3">
+          <div className="rounded lg:p-5 lg:transition lg:duration-300 lg:hover:bg-indigo-50">
+            <div className="flex items-center mb-1">
+              <span className="flex items-center justify-center w-4 h-4 mr-2 text-xs font-medium text-white rounded bg-deep-purple-accent-400">
+                1
+              </span>
+              <p className="text-lg font-semibold sm:text-base">
+                Read the recipe
+              </p>
+            </div>
+            <p className="text-sm text-gray-900">
+              Take a good look at the recipe. Don’t just skim it; read it
+              through from start to finish. As you read, visualize doing the
+              steps which will help when you’re prepping.
+            </p>
+          </div>
+          <div className="rounded lg:p-5 lg:transition lg:duration-300 lg:hover:bg-teal-50">
+            <div className="flex items-center mb-1">
+              <span className="flex items-center justify-center w-4 h-4 mr-2 text-xs font-medium text-teal-900 rounded bg-teal-accent-400">
+                2
+              </span>
+              <p className="text-lg font-semibold sm:text-base">
+                Know the assumptions
+              </p>
+            </div>
+            <p className="text-sm text-gray-900">
+              All recipes are written using certain conventions, which define
+              the characteristics of common ingredients. The rules vary from
+              place to place.
+            </p>
+          </div>
+          <div className="rounded lg:p-5 lg:transition lg:duration-300 lg:hover:bg-teal-50">
+            <div className="flex items-center mb-1">
+              <span className="flex items-center justify-center w-4 h-4 mr-2 text-xs font-medium text-white rounded md:text-teal-900 bg-deep-purple-accent-400 md:bg-teal-accent-400">
+                3
+              </span>
+              <p className="text-lg font-semibold sm:text-base">
+                Figure out the timing
+              </p>
+            </div>
+            <p className="text-sm text-gray-900">
+              Check the “prep time” and “total time” listed at the top to be
+              sure you have enough time to complete the recipe. Look for hints,
+              such as the words “meanwhile” .
+            </p>
+          </div>
+          <div className="rounded lg:p-5 lg:transition lg:duration-300 lg:hover:bg-indigo-50">
+            <div className="flex items-center mb-1">
+              <span className="flex items-center justify-center w-4 h-4 mr-2 text-xs font-medium text-teal-900 rounded md:text-white bg-teal-accent-400 md:bg-deep-purple-accent-400">
+                4
+              </span>
+              <p className="text-lg font-semibold sm:text-base">Plan ahead</p>
+            </div>
+            <p className="text-sm text-gray-900">
+              Missing a prep instruction can leave you scrambling in the middle
+              of a recipe. Keep your eyes peeled for time-consuming steps and be
+              careful.
+            </p>
+          </div>
+        </div>
+        <div className="relative md:col-span-2 lg:col-span-2">
+          <img
+            className="inset-0 object-cover object-bottom w-full h-56 rounded shadow-lg lg:absolute lg:h-full"
+            src="https://images.pexels.com/photos/3182759/pexels-photo-3182759.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+        <div>
+          <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
+            Brand new
+          </p>
+        </div>
+        <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+          <span className="relative inline-block">
+            <svg
+              viewBox="0 0 52 24"
+              fill="currentColor"
+              className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
+            >
+              <defs>
+                <pattern
+                  id="9a29985a-fc16-419b-ae53-1670f5ca4491"
+                  x="0"
+                  y="0"
+                  width=".135"
+                  height=".30"
+                >
+                  <circle cx="1" cy="1" r=".7" />
+                </pattern>
+              </defs>
+              <rect
+                fill="url(#9a29985a-fc16-419b-ae53-1670f5ca4491)"
+                width="52"
+                height="24"
+              />
+            </svg>
+            <span className="relative">The</span>
+          </span>{' '}
+          quick, brown fox jumps over a lazy dog
+        </h2>
+        <p className="text-base text-gray-700 md:text-lg">
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque rem aperiam, eaque ipsa quae.
+        </p>
+      </div>
+      <div className="grid max-w-screen-lg gap-8 row-gap-5 mb-8 sm:grid-cols-2 lg:grid-cols-4 sm:mx-auto">
+        <img
+          className="object-cover w-full h-56 rounded shadow-lg"
+          src="https://images.pexels.com/photos/3184291/pexels-photo-3184291.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+          alt=""
+        />
+        <img
+          className="object-cover w-full h-56 rounded shadow-lg"
+          src="https://images.pexels.com/photos/3182746/pexels-photo-3182746.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+          alt=""
+        />
+        <img
+          className="object-cover w-full h-56 rounded shadow-lg"
+          src="https://images.pexels.com/photos/3184311/pexels-photo-3184311.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;w=500"
+          alt=""
+        />
+        <img
+          className="object-cover w-full h-56 rounded shadow-lg"
+          src="https://images.pexels.com/photos/3184339/pexels-photo-3184339.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+          alt=""
+        />
+      </div>
+      <div className="flex items-center sm:justify-center">
+        <a
+          href="/"
+          className="inline-flex items-center justify-center h-12 px-6 mr-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+        >
+          Sign up
+        </a>
+        <a
+          href="/"
+          aria-label=""
+          className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+        >
+          Learn more
+        </a>
+      </div>
+    </div>
+    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="grid gap-5 row-gap-10 lg:grid-cols-2">
+        <div className="flex flex-col justify-center">
+          <div className="max-w-xl mb-6">
+            <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+              The quick, brown fox
+              <br className="hidden md:block" />
+              jumps over{' '}
+              <span className="relative px-1">
+                <div className="absolute inset-x-0 bottom-0 h-3 transform -skew-x-12 bg-teal-accent-400" />
+                <span className="relative inline-block text-deep-purple-accent-400">
+                  a lazy dog
+                </span>
+              </span>
+            </h2>
+            <p className="text-base text-gray-700 md:text-lg">
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae. explicabo.
+            </p>
+          </div>
+          <p className="mb-4 text-sm font-bold tracking-widest uppercase">
+            Features
+          </p>
+          <div className="grid space-y-3 sm:gap-2 sm:grid-cols-2 sm:space-y-0">
+            <ul className="space-y-3">
+              <li className="flex">
+                <span className="mr-1">
+                  <svg
+                    className="w-5 h-5 mt-px text-deep-purple-accent-400"
+                    stroke="currentColor"
+                    viewBox="0 0 52 52"
+                  >
+                    <polygon
+                      strokeWidth="4"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      fill="none"
+                      points="29 13 14 29 25 29 23 39 38 23 27 23"
+                    />
+                  </svg>
+                </span>
+                A slice of heaven
+              </li>
+              <li className="flex">
+                <span className="mr-1">
+                  <svg
+                    className="w-5 h-5 mt-px text-deep-purple-accent-400"
+                    stroke="currentColor"
+                    viewBox="0 0 52 52"
+                  >
+                    <polygon
+                      strokeWidth="4"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      fill="none"
+                      points="29 13 14 29 25 29 23 39 38 23 27 23"
+                    />
+                  </svg>
+                </span>
+                Disrupt inspire
+              </li>
+              <li className="flex">
+                <span className="mr-1">
+                  <svg
+                    className="w-5 h-5 mt-px text-deep-purple-accent-400"
+                    stroke="currentColor"
+                    viewBox="0 0 52 52"
+                  >
+                    <polygon
+                      strokeWidth="4"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      fill="none"
+                      points="29 13 14 29 25 29 23 39 38 23 27 23"
+                    />
+                  </svg>
+                </span>
+                Preliminary thinking
+              </li>
+            </ul>
+            <ul className="space-y-3">
+              <li className="flex">
+                <span className="mr-1">
+                  <svg
+                    className="w-5 h-5 mt-px text-deep-purple-accent-400"
+                    stroke="currentColor"
+                    viewBox="0 0 52 52"
+                  >
+                    <polygon
+                      strokeWidth="4"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      fill="none"
+                      points="29 13 14 29 25 29 23 39 38 23 27 23"
+                    />
+                  </svg>
+                </span>
+                Flipboard curmudgeon
+              </li>
+              <li className="flex">
+                <span className="mr-1">
+                  <svg
+                    className="w-5 h-5 mt-px text-deep-purple-accent-400"
+                    stroke="currentColor"
+                    viewBox="0 0 52 52"
+                  >
+                    <polygon
+                      strokeWidth="4"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      fill="none"
+                      points="29 13 14 29 25 29 23 39 38 23 27 23"
+                    />
+                  </svg>
+                </span>
+                Storage shed
+              </li>
+              <li className="flex">
+                <span className="mr-1">
+                  <svg
+                    className="w-5 h-5 mt-px text-deep-purple-accent-400"
+                    stroke="currentColor"
+                    viewBox="0 0 52 52"
+                  >
+                    <polygon
+                      strokeWidth="4"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      fill="none"
+                      points="29 13 14 29 25 29 23 39 38 23 27 23"
+                    />
+                  </svg>
+                </span>
+                Satoshi Nakamoto
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <img
+            className="object-cover w-full h-56 rounded shadow-lg sm:h-96"
+            src="https://images.pexels.com/photos/927022/pexels-photo-927022.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+    </>
   )
 }
 
