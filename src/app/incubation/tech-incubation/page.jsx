b0967f1398fa3fa47f7ac517dfd7b0f2da5f9c76
ADITYA TYAGI <adityatyagiav1@gmail.com>
2023-09-07 21:34:50 +0530

    component Scheme designed

--- src/app/incubation/tech-incubation/page.jsx
+++ src/app/incubation/tech-incubation/page.jsx
@@ -1,79 +1,14 @@
-"use client"
+
+import Scheme from '@/components/schemes'
 import React from 'react'
 
 
 function page() {
     return (
        <>
-     
-     <h1 className="text-2xl font-semibold text-center mt-12 text-gray-800 capitalize lg:text-4xl dark:text-white">NIDHI EIR <span className="text-blue-900">1.0</span></h1>
-
-    <div class="lg:flex my-6">
-        
-        <div class="flex items-center justify-center w-full px-6 py-8 lg:h-[32rem] lg:w-1/2">
-            <div class="max-w-xl">
-                <h2 class="text-3xl font-semibold text-gray-800 dark:text-white lg:text-4xl">Build Your New <span class="text-blue-600 dark:text-blue-400">Idea</span></h2>
-
-                <p class="mt-4 text-sm text-gray-500 dark:text-gray-400 lg:text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis commodi cum cupiditate ducimus, fugit harum id necessitatibus odio quam quasi, quibusdam rem tempora voluptates.</p>
-
-                <div class="flex flex-col mt-6 space-y-3 lg:space-y-0 lg:flex-row">
-                    <a href="#" class="block px-5 py-2 text-sm font-medium tracking-wider text-center text-white transition-colors duration-300 transform bg-blue-950 rounded-md hover:bg-blue-800">Register Here</a>
-                    <a href="#" class="block px-5 py-2 text-sm font-medium tracking-wider text-center text-gray-700 transition-colors duration-300 transform bg-gray-200 rounded-md lg:mx-4 hover:bg-gray-300">Learn More</a>
-                </div>
-            </div>
-        </div>
-
-        <div class="w-full h-64 lg:w-1/2 lg:h-auto">
-            <div class="w-full h-full bg-cover" style={{backgroundImage: `url(https://images.unsplash.com/photo-1508394522741-82ac9c15ba69?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=748&q=80)`}}>
-                <div class="w-full h-full bg-black opacity-25"></div>
-            </div>
-        </div>
-    </div>
-    
-    <h1 className="text-2xl font-semibold text-center mt-24 text-gray-800 capitalize lg:text-4xl dark:text-white">NIDHI EIR <span className="text-blue-900">1.0</span></h1>
-    <div class="lg:flex lg:flex-row-reverse my-6">
-        <div class="flex items-center justify-center w-full px-6 py-8 lg:h-[32rem] lg:w-1/2">
-            <div class="max-w-xl">
-                <h2 class="text-3xl font-semibold text-gray-800 dark:text-white lg:text-4xl">Build Your New <span class="text-blue-600 dark:text-blue-400">Idea</span></h2>
-
-                <p class="mt-4 text-sm text-gray-500 dark:text-gray-400 lg:text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis commodi cum cupiditate ducimus, fugit harum id necessitatibus odio quam quasi, quibusdam rem tempora voluptates.</p>
-
-                <div class="flex flex-col mt-6 space-y-3 lg:space-y-0 lg:flex-row">
-                    <a href="#" class="block px-5 py-2 text-sm font-medium tracking-wider text-center text-white transition-colors duration-300 transform bg-gray-900 rounded-md hover:bg-gray-700">Register Here</a>
-                    <a href="#" class="block px-5 py-2 text-sm font-medium tracking-wider text-center text-gray-700 transition-colors duration-300 transform bg-gray-200 rounded-md lg:mx-4 hover:bg-gray-300">Learn More</a>
-                </div>
-            </div>
-        </div>
-
-        <div class="w-full h-64 lg:w-1/2 lg:h-auto">
-            <div class="w-full h-full bg-cover" style={{backgroundImage: `url(https://images.unsplash.com/photo-1508394522741-82ac9c15ba69?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=748&q=80)`}}>
-                <div class="w-full h-full bg-black opacity-25"></div>
-            </div>
-        </div>
-    </div>
-    <h1 className="text-2xl font-semibold text-center mt-24 text-gray-800 capitalize lg:text-4xl dark:text-white">NIDHI EIR <span className="text-blue-900">1.0</span></h1>
-    <div class="lg:flex my-6">
-        <div class="flex items-center justify-center w-full px-6 py-8 lg:h-[32rem] lg:w-1/2">
-            <div class="max-w-xl">
-                <h2 class="text-3xl font-semibold text-gray-800 dark:text-white lg:text-4xl">Build Your New <span class="text-blue-600 dark:text-blue-400">Idea</span></h2>
-
-                <p class="mt-4 text-sm text-gray-500 dark:text-gray-400 lg:text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis commodi cum cupiditate ducimus, fugit harum id necessitatibus odio quam quasi, quibusdam rem tempora voluptates.</p>
-
-                <div class="flex flex-col mt-6 space-y-3 lg:space-y-0 lg:flex-row">
-                    <a href="#" class="block px-5 py-2 text-sm font-medium tracking-wider text-center text-white transition-colors duration-300 transform bg-gray-900 rounded-md hover:bg-gray-700">Register Here</a>
-                    <a href="#" class="block px-5 py-2 text-sm font-medium tracking-wider text-center text-gray-700 transition-colors duration-300 transform bg-gray-200 rounded-md lg:mx-4 hover:bg-gray-300">Learn More</a>
-                </div>
-            </div>
-        </div>
-
-        <div class="w-full h-64 lg:w-1/2 lg:h-auto">
-            <div class="w-full h-full bg-cover" style={{backgroundImage: `url(https://images.unsplash.com/photo-1508394522741-82ac9c15ba69?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=748&q=80)`}}>
-                <div class="w-full h-full bg-black opacity-25"></div>
-            </div>
-        </div>
-    </div>
-
-
+       <Scheme/>
+     <Scheme direction="lg:flex-row-reverse"/>
+     <Scheme/>
        </>
     )
 }
