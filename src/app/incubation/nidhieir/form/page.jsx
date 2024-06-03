4180bb6c7d748418ddf25903e9fea796faad42b8
adarsh1278 <120383844+adarsh1278@users.noreply.github.com>
2024-06-03 16:58:06 +0530

    select bar added

--- src/app/incubation/nidhieir/form/page.jsx
+++ src/app/incubation/nidhieir/form/page.jsx
@@ -13,14 +13,12 @@ import InputRadio from '@/components/inputRadio';
 import InputTextArea from '@/components/inputTextArea';
 import InputFile from '@/components/inputFile';
 import FinalReview from './finalreview';
+import { MenuItem, Select, FormControl, InputLabel, FormHelperText } from '@mui/material';
 
-
-const steps = ['Applicant Details', 'Idea Description', 'Checklist','Final Review'];
+const steps = ['Applicant Details', 'Idea Description', 'Checklist', 'Final Review'];
 
 export default function HorizontalLinearStepper() {
   const [activeStep, setActiveStep] = React.useState(0);
-
- 
 
   const handleNext = () => {
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
@@ -30,18 +28,18 @@ export default function HorizontalLinearStepper() {
     setActiveStep((prevActiveStep) => prevActiveStep - 1);
   };
 
- 
-  //conditions for dob
+  // conditions for dob
   const today = new Date();
   const minDate = new Date(today);
   minDate.setFullYear(today.getFullYear() - 15);
 
-  //validation Schema
+  // validation Schema
   const validationSchema = Yup.object({
     name: Yup.string()
       .max(15, 'Must be 15 characters or less')
       .required('Required'),
     email: Yup.string().email('Invalid email address').required('Required'),
+    scheme: Yup.string().required('Required'), // Add validation for scheme
     phoneNumber: Yup.string()
       .matches(/^[0-9]{10}$/, '10 digit mobile number should be entered')
       .required('Required'),
@@ -84,15 +82,14 @@ export default function HorizontalLinearStepper() {
     businessCommitment: Yup.string().required('Required'),
     notBeneficiary: Yup.string().required('Required'),
     registerPEP: Yup.string().required('Required'),
-
   });
-
 
   const formik = useFormik({
     initialValues: {
       name: '',
       phoneNumber: '',
       email: '',
+      scheme: '', // Add initial value for scheme
       dateOfBirth: '',
       gender: '',
       category: '',
@@ -114,38 +111,40 @@ export default function HorizontalLinearStepper() {
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2));
       console.log(JSON.stringify(values, null, 2));
-      // console.log(JSON.stringify(values, null, 2));
-
     },
   });
+
   const genderOptions = [
     { label: 'Male', value: 'male' },
     { label: 'Female', value: 'female' },
     { label: 'Prefer Not to Say', value: 'prefernottosay' },
   ];
-  const categoryOptions = [
 
+  const categoryOptions = [
     { label: 'General', value: 'general' },
     { label: 'OBC', value: 'obc' },
     { label: 'SC', value: 'sc' },
     { label: 'ST', value: 'st' },
-
-  ]
+  ];
 
   const generalOptions = [
     { label: 'Yes', value: 'yes' },
     { label: 'No', value: 'no' },
-  ]
+  ];
+
+  const schemeOptions = [
+    { label: 'Scheme1', value: 'scheme1' },
+    { label: 'Scheme2', value: 'scheme2' },
+    { label: 'Scheme3', value: 'scheme3' },
+  ];
 
   return (
     <Box sx={{ width: '81%' }} className="mx-auto">
-       <form onSubmit={formik.handleSubmit}>
+      <form onSubmit={formik.handleSubmit}>
         <Stepper activeStep={activeStep} className='hidden sm:flex'>
           {steps.map((label) => {
             const stepProps = {};
             const labelProps = {};
-
-            
             return (
               <Step key={label} {...stepProps}>
                 <StepLabel {...labelProps}>{label}</StepLabel>
@@ -154,22 +153,20 @@ export default function HorizontalLinearStepper() {
           })}
         </Stepper>
 
-        {activeStep === steps.length-1 &&
+        {activeStep === steps.length - 1 &&
           <>
             <div className="mt-5 mb-4 mx-auto">
-                <h1 className="text-2xl font-semibold text-gray-800 capitalize mx-auto lg:text-3xl ">
+              <h1 className="text-2xl font-semibold text-gray-800 capitalize mx-auto lg:text-3xl ">
                 {`FINAL REVIEW`}
-                </h1>
+              </h1>
 
-                <div className="flex mx-auto mt-2">
-                    <span className="inline-block w-40 h-1 bg-blue-900 rounded-full"></span>
-                    <span className="inline-block w-3 h-1 mx-1 bg-blue-900 rounded-full"></span>
-                    <span className="inline-block w-1 h-1 bg-blue-900 rounded-full"></span>
-                </div>
-
+              <div className="flex mx-auto mt-2">
+                <span className="inline-block w-40 h-1 bg-blue-900 rounded-full"></span>
+                <span className="inline-block w-3 h-1 mx-1 bg-blue-900 rounded-full"></span>
+                <span className="inline-block w-1 h-1 bg-blue-900 rounded-full"></span>
+              </div>
             </div>
-            <FinalReview formik={formik}/>
-            
+            <FinalReview formik={formik} />
 
             <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
               <Button
@@ -181,52 +178,62 @@ export default function HorizontalLinearStepper() {
                 Back
               </Button>
               <Box sx={{ flex: '1 1 auto' }} />
-
-
-              <Button type='submit' >Finish</Button>
+              <Button type='submit'>Finish</Button>
             </Box>
-
-            
           </>
         }
 
-
-
         {activeStep === 0 &&
-
           <>
-          <div className="mt-8"/>
-          <div className="mt-5 mb-4">
-                <h2 className="text-xl font-semibold text-gray-800 capitalize lg:text-2xl ">
+            <div className="mt-8" />
+            <div className="mt-5 mb-4">
+              <h2 className="text-xl font-semibold text-gray-800 capitalize lg:text-2xl ">
                 {`Applicant Details`}
-                </h2>
+              </h2>
 
-                <div className="flex mx-auto mt-2">
-                    <span className="inline-block w-40 h-1 bg-blue-900 rounded-full"></span>
-                    <span className="inline-block w-3 h-1 mx-1 bg-blue-900 rounded-full"></span>
-                    <span className="inline-block w-1 h-1 bg-blue-900 rounded-full"></span>
-                </div>
+              <div className="flex mx-auto mt-2">
+                <span className="inline-block w-40 h-1 bg-blue-900 rounded-full"></span>
+                <span className="inline-block w-3 h-1 mx-1 bg-blue-900 rounded-full"></span>
+                <span className="inline-block w-1 h-1 bg-blue-900 rounded-full"></span>
+              </div>
             </div>
             <Input className="mt-4" value="name" label="1. Name Of the Applicant" placeHolder="Aditya Tyagi" formikTouched={formik.touched.name} formikError={formik.errors.name} formikChange={formik.handleChange} formikBlur={formik.handleBlur} formikValue={formik.values.name} type="text" />
 
             <Input value="email" label="2. Your Email" placeHolder="adityatyagi@gmail.com" formikTouched={formik.touched.email} formikError={formik.errors.email} formikChange={formik.handleChange} formikBlur={formik.handleBlur} formikValue={formik.values.email} type="email" />
 
-            <Input value="phoneNumber" label="3. Phone/Mobile Number" placeHolder="8088088088" formikTouched={formik.touched.phoneNumber} formikError={formik.errors.phoneNumber} formikChange={formik.handleChange} formikBlur={formik.handleBlur} formikValue={formik.values.phoneNumber} type="tel" />
+            {/* Add Scheme Dropdown */}
+            <div  className=' flex w-full'>
+              <div className=' p-4 pl-0 text-sm text-center mt-3'>3</div>
+            <FormControl fullWidth margin="normal" error={formik.touched.scheme && Boolean(formik.errors.scheme)}>
+            <InputLabel id="scheme-label" > Select Scheme</InputLabel>
+              <Select
+                labelId="scheme-label"
+                id="scheme"
+                name="scheme"
+                value={formik.values.scheme}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+               
+              >
+                {schemeOptions.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                  </MenuItem>
+                ))}
+              </Select>
+              <FormHelperText>{formik.touched.scheme && formik.errors.scheme}</FormHelperText>
+            </FormControl>
+            </div>
 
-            <InputTextArea value="address" label="4. Full Postal Address" placeHolder="5A, street-12,Thane Mumbai " formikTouched={formik.touched.address} formikError={formik.errors.address} formikChange={formik.handleChange} formikBlur={formik.handleBlur} formikValue={formik.values.address} />
+            <Input value="phoneNumber" label="4. Mobile Number" placeHolder="9876543210" formikTouched={formik.touched.phoneNumber} formikError={formik.errors.phoneNumber} formikChange={formik.handleChange} formikBlur={formik.handleBlur} formikValue={formik.values.phoneNumber} type="text" />
+            <Input value="dateOfBirth" label="5. Date of Birth" placeHolder="1998-06-30" formikTouched={formik.touched.dateOfBirth} formikError={formik.errors.dateOfBirth} formikChange={formik.handleChange} formikBlur={formik.handleBlur} formikValue={formik.values.dateOfBirth} type="date" />
+            <InputRadio value="gender" label="6. Gender" formikTouched={formik.touched.gender} formikError={formik.errors.gender} formikChange={formik.handleChange} formikBlur={formik.handleBlur} formikValue={formik.values.gender} type="text" options={genderOptions} />
+            <InputRadio value="category" label="7. Category" formikTouched={formik.touched.category} formikError={formik.errors.category} formikChange={formik.handleChange} formikBlur={formik.handleBlur} formikValue={formik.values.category} type="text" options={categoryOptions} />
+            <InputTextArea value="address" label="8. Address" placeHolder="Type your address here..." formikTouched={formik.touched.address} formikError={formik.errors.address} formikChange={formik.handleChange} formikBlur={formik.handleBlur} formikValue={formik.values.address} />
+            <Input value="education" label="9. Educational Qualifications" placeHolder="Type your educational qualifications here..." formikTouched={formik.touched.education} formikError={formik.errors.education} formikChange={formik.handleChange} formikBlur={formik.handleBlur} formikValue={formik.values.education} type="text" />
+            <InputTextArea value="experience" label="10. Work Experience" placeHolder="Type your work experience here..." formikTouched={formik.touched.experience} formikError={formik.errors.experience} formikChange={formik.handleChange} formikBlur={formik.handleBlur} formikValue={formik.values.experience} />
 
-            <Input value="dateOfBirth" label="5. Date Of Birth" placeHolder="02-06-2003" formikTouched={formik.touched.dateOfBirth} formikError={formik.errors.dateOfBirth} formikChange={formik.handleChange} formikBlur={formik.handleBlur} formikValue={formik.values.dateOfBirth} type="date" />
-
-            <InputRadio value="gender" label="6. Gender" options={genderOptions} formikTouched={formik.touched.gender} formikError={formik.errors.gender} formikChange={formik.handleChange} formikBlur={formik.handleBlur} />
-
-            <InputRadio value="category" label="7. Category" options={categoryOptions} formikTouched={formik.touched.category} formikError={formik.errors.category} formikChange={formik.handleChange} formikBlur={formik.handleBlur} />
-
-            <InputTextArea value="education" label="8. Basic undergraduate training/education" placeHolder="Enter your educational Details " formikTouched={formik.touched.education} formikError={formik.errors.education} formikChange={formik.handleChange} formikBlur={formik.handleBlur} formikValue={formik.values.education} />
-
-            <InputTextArea value="experience" label="9. Any experiences relating to entrepreneurship, leadership, fund raising, organizing activities/ events etc." placeHolder="Your Experience " formikTouched={formik.touched.experience} formikError={formik.errors.experience} formikChange={formik.handleChange} formikBlur={formik.handleBlur} formikValue={formik.values.experience} />
-
-            <InputFile value="resume" label="10. Attach CV or Resume with details of education and work experience" formik={formik} formikTouched={formik.touched.resume} formikError={formik.errors.resume} formikChange={formik.handleChange} formikBlur={formik.handleBlur} formikValue={formik.values.resume} />
-
+            <InputFile value="resume" label="11. Resume/CV" formikTouched={formik.touched.resume} formikError={formik.errors.resume} formikChange={(event) => formik.setFieldValue('resume', event.currentTarget.files[0])} formikBlur={formik.handleBlur} formikValue={formik.values.resume} />
 
             <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
               <Button
@@ -238,13 +245,7 @@ export default function HorizontalLinearStepper() {
                 Back
               </Button>
               <Box sx={{ flex: '1 1 auto' }} />
-
-
-              <Button
-                onClick={handleNext}>
-
-                {'Next'}
-              </Button>
+              <Button onClick={handleNext}>Next</Button>
             </Box>
           </>
         }
